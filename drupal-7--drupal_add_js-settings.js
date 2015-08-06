9809e3815567c5feb676860976baeca70c9f7c89
yann <yann.boisselier@gmail.com>
2015-08-06 17:21:46 +0200

    up

---
+++ drupal-7--drupal_add_js-settings.js
@@ -0,0 +1,4 @@
+@desc add a drupal js settings array
+@tags drupal 7, drupal_add_js
+
+drupal_add_js(array('pageRebranding' => $branding), array('type' => 'setting'));
---
+++ php--cors.php
@@ -0,0 +1,10 @@
+@desc enable cors for json webservices for example
+@tags cors, webservice
+
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true ");
+header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
+header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, 
+    X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
+
