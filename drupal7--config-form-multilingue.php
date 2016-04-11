041d37d4f47e9a8caba7a8725f20f6f518233719
yann <yann.boisselier@gmail.com>
2016-04-11 17:54:52 +0200

    ajout snippet

---
+++ drupal7--config-form-multilingue.php
@@ -0,0 +1,27 @@
+<?php
+
+function pdt_cookie_consent_form() {
+
+  $form = array();
+
+  $languages = language_list();
+  foreach ($languages as $language) {
+
+    $lang = $language->language;
+    $form[$lang] = array(
+      '#type' => "fieldset",
+      '#title' => "Configuration pour le language " . $lang,
+    );
+
+    $form[$lang]["pdt_cookie_consent_message_$lang"] = array(
+      '#title' => 'Message',
+      '#type' => 'textfield',
+      '#description' => '',
+      '#default_value' =>  variable_get("pdt_cookie_consent_message_$lang", "")
+    );
+  }
+
+  return system_settings_form($form);
+
+}
+
