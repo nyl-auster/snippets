238f4403bce36f5b68e7147075a25cfe072b20e0
yann <yann.boisselier@gmail.com>
2016-04-19 17:13:20 +0200

    add drupal behaviors js

---
+++ drupal-7--behaviors.js
@@ -0,0 +1,8 @@
+(function ($) {
+  Drupal.behaviors.exampleModule = {
+    attach: function (context, settings) {
+              // Code to be run on page load, and
+              // on ajax load added here
+            }
+  };
+}(jQuery));
