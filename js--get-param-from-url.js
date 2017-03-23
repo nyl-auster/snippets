5633c0bebe6ab6401ec32d9baf28a835967febca
yann <yann.boisselier@gmail.com>
2017-03-23 15:23:01 +0100

    js : getUrlFromParam

---
+++ js--get-param-from-url.js
@@ -0,0 +1,11 @@
+
+function getParamsFromUrl() {
+  var params = [];
+  if (location.search.indexOf('?') === -1) return [];
+  location.search.substring(1).split('&').map(function(item){
+    var parts = item.split('=');
+    params[parts[0]] = parts[1];
+  });
+  return params;
+}
+
