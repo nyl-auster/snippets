66ec1dd5035fbdb2249023796533c3cae6f133e1
yann <yann.boisselier@gmail.com>
2017-03-23 15:34:06 +0100

    js get params from url

--- js--get-param-from-url.js
+++ js--get-param-from-url.js
@@ -1,11 +1,11 @@
 
-function getParamsFromUrl() {
+function getParamsFromUrl(param) {
   var params = [];
   if (location.search.indexOf('?') === -1) return [];
   location.search.substring(1).split('&').map(function(item){
     var parts = item.split('=');
     params[parts[0]] = parts[1];
   });
-  return params;
+  return param ? params[param] : params;
 }
 
